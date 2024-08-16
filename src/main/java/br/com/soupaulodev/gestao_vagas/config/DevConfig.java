ba6549b53a59f542e83a18981d15f7fdb94edbff
Paulo Marques <paulomarques.rs1@gmail.com>
2024-08-15 23:30:29 -0300

    fix(seed-config-db): fixing seed to dev db

--- src/main/java/br/com/soupaulodev/gestao_vagas/config/DevConfig.java
+++ src/main/java/br/com/soupaulodev/gestao_vagas/config/DevConfig.java
@@ -2,6 +2,12 @@ package br.com.soupaulodev.gestao_vagas.config;
 
 import br.com.soupaulodev.gestao_vagas.modules.candidate.entities.CandidateEntity;
 import br.com.soupaulodev.gestao_vagas.modules.candidate.repositories.CandidateRepository;
+import br.com.soupaulodev.gestao_vagas.modules.candidate.useCases.CreateCandidateUseCase;
+import br.com.soupaulodev.gestao_vagas.modules.candidate.useCases.ProfileCandidateUseCase;
+import br.com.soupaulodev.gestao_vagas.modules.company.entities.CompanyEntity;
+import br.com.soupaulodev.gestao_vagas.modules.company.repositories.CompanyRepository;
+import br.com.soupaulodev.gestao_vagas.modules.company.useCases.CreateCompanyUseCase;
+import br.com.soupaulodev.gestao_vagas.modules.company.useCases.GetCompanyUseCase;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -14,21 +20,25 @@ import java.util.Optional;
 public class DevConfig implements CommandLineRunner {
 
     private final CandidateRepository candidateRepository;
+    private final CompanyRepository companyRepository;
 
-    public DevConfig(CandidateRepository candidateRepository) {
+    public DevConfig(
+        CandidateRepository candidateRepository,
+        CompanyRepository companyRepository
+    ) {
         this.candidateRepository = candidateRepository;
+        this.companyRepository = companyRepository;
     }
 
     @Override
     public void run(String... args) {
-        Optional<CandidateEntity> seeded = this.candidateRepository.findByUsername("username0");
 
-        if (!seeded.isPresent()) {
+        if (!this.candidateRepository.findByUsername("username0").isPresent()) {
             for (int i = 0; i < 10; i++) {
 
                 CandidateEntity candidate = new CandidateEntity(
                         null,
-                        "Candidate " + i,
+                        "Candidate name " + i,
                         "username" + i,
                         "email" + i + "@gmail.com",
                         "password" + i,
@@ -38,6 +48,22 @@ public class DevConfig implements CommandLineRunner {
                 );
 
                 this.candidateRepository.save(candidate);
+            }
+        }
+
+        if (!this.companyRepository.findByUsername("username0").isPresent()) {
+            for (int i = 0; i < 10; i++) {
+                CompanyEntity company = new CompanyEntity(
+                        null,
+                        "Company name " + i,
+                        "username" + i,
+                        "email" + i + "@gmail.com",
+                        "password" + i,
+                        "https://website" + i + ".com",
+                        LocalDateTime.now()
+                );
+
+                this.companyRepository.save(company);
             }
         }
     }
